f71b35521e79b524277522069360352498e6b987
Max24p7 <82169517+Max24p7@users.noreply.github.com>
2023-06-29 10:18:17 -0500

    Added function for generating password (INCOMPLETE)

--- script.js
+++ script.js
@@ -1,5 +1,61 @@
 // Assignment code here
 
+// Variables for password criteria types
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var symbols = "!@#$%^&*()-_=+[]{};:,<>./?";
+
+// Function for generating a random password
+
+// Asks user how long they would like their password to be.
+function generatePassword() {
+  var pwLength = prompt("What would you like the length of your password to be? (Please choose a number between 8-128)");
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Try again. Length must be between 8 and 128.");
+    return;
+  }
+
+  var pwCriteria = "";
+
+  //Series of prompts to ask which character types the user would like to use in their password.
+  //lowercase prompt
+  var includeLowerCase = confirm("Would you like to include lowercase letters?");
+  if (includeLowerCase) {
+    pwCriteria += lowerCase;
+  }
+
+  //uppercase prompt
+  var includeUpperCase = confirm("Would you like to include uppercase letters?");
+  if (includeUpperCase) {
+    pwCriteria += upperCase;
+  }
+
+  //number prompt
+  var includeNumbers = confirm("Would you like to include numbers?");
+  if (includeNumbers) {
+    pwCriteria += numbers;
+  }
+
+  //symbols prompt
+  var includeSymbols = confirm("Would you like to include symbols and special characters?");
+  if (includeSymbols) {
+    pwCriteria += symbols;
+  }
+
+  //Confirms that at least one type was selected
+  // ?? not functioning as !== ??
+  if (pwCriteria === "") {
+    alert("At least one character class must be selected.");
+    return;
+  }
+
+  //stores generated password
+  var password = "";
+
+  
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
